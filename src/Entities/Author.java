fa3cd3137e457e3d114afe84a3189da7431e0e37
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-06 21:46:22 -0300

    feat: Implement Author Entitie

--- src/Entities/Author.java
+++ src/Entities/Author.java
@@ -1,4 +1,45 @@
 package Entities;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class Author {
+    private final String id;
+    private String name;
+    private String birthDate;
+
+    public Author(String name, String birthDate) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "\n id = " + id +
+                ",\n nome = " + name +
+                ",\n birthDate =" + birthDate +
+                "\n}";
+    }
 }
