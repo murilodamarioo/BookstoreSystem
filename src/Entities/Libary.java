dacdd27b11a0934ac59ea5bd1611560300b30f85
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-06 22:53:03 -0300

    feat: Implement Libary entitie

--- src/Entities/Libary.java
+++ src/Entities/Libary.java
@@ -1,4 +1,28 @@
 package Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libary {
+    private List<Book> books = new ArrayList<>();
+    private List<Author> authors = new ArrayList<>();
+    private List<Loan> loans = new ArrayList<>();
+
+    public Libary(List<Book> books, List<Author> authors, List<Loan> loans) {
+        this.books = books;
+        this.authors = authors;
+        this.loans = loans;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public List<Loan> getLoans() {
+        return loans;
+    }
 }
